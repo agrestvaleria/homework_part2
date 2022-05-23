70c36f332b67734ceaca07eba147a486ad6b4794
Valeria Agrest <agrestval@gmail.com>
2022-05-23 18:30:06 +0300

    service args after reinstall chrome & webdriver 20

--- tests_j/test_bla.py
+++ tests_j/test_bla.py
@@ -1,19 +1,15 @@
 from selenium import webdriver
 import pytest
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='class')
 def web_driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-setuid-sandbox")
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path='/home/valerya/PycharmProjects/My_homework/tests/tests_j/chromedriver')
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
