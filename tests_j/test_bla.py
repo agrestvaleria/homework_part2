e344c5c859fdce4fe34644c9eef74f7c5628c358
Valeria Agrest <agrestval@gmail.com>
2022-05-23 18:12:25 +0300

    service args after reinstall chrome & webdriver 18

--- tests_j/test_bla.py
+++ tests_j/test_bla.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
 import pytest
 
+# from xvfbwrapper import Xvfb
+from selenium.webdriver.chrome.options import Options
+
 @pytest.fixture(scope='class')
 def web_driver():
-    chrome_options = webdriver.ChromeOptions()
+    # vdisplay = Xvfb()
+    # vdisplay.start()
+    chrome_options = Options()
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='./chromedriver')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
-
+    # vdisplay.stop()
 
 class TestGoogle:
 
