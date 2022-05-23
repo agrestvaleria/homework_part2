ab35202989051b321c9e25b910332e11925b7320
Valeria Agrest <agrestval@gmail.com>
2022-05-23 19:10:22 +0300

    service args after reinstall chrome & webdriver 21

--- tests_j/test_bla.py
+++ tests_j/test_bla.py
@@ -1,15 +1,20 @@
+import warnings
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='class')
 def web_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
+    chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=chrome_options,
-                              executable_path='/home/valerya/PycharmProjects/My_homework/tests/tests_j/chromedriver')
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
+                              options=chrome_options)
+    driver.set_window_size(1920, 1080)
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
