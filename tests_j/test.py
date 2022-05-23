15c9f39fb3025c5b5b41b0cd56c596b7bc85ceb9
Valeria Agrest <agrestval@gmail.com>
2022-05-23 17:06:33 +0300

    service args after reinstall chrome & webdriver 6

--- tests_j/test.py
+++ tests_j/test.py
@@ -1,27 +1,16 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from tempfile import mkdtemp
 
 @pytest.fixture(scope='session')
 def web_driver():
-    options = Options()
-    options.binary_location = "/usr/bin/google-chrome-stable"
-    options.add_argument('--headless')
+    options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--window-size=1280x1696")
-    # options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--disable-dev-tools")
-    # options.add_argument("--no-zygote")
-    # options.add_argument(f"--user-data-dir={mkdtemp()}")
-    # options.add_argument(f"--data-path={mkdtemp()}")
-    # options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
-    # driver = webdriver.Chrome( options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
+    options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(chrome_options=options, executable_path="/home/valerya/PycharmProjects/My_homework/tests/tests_j/chromedriver")
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
